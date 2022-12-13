c39fbdd1a158e94162af2be3d80e1b35a9e7317b
oguzhanzeybek <darkstroge@gmail.com>
2022-12-13 23:04:09 +0300

    Hello World
    tahmin edin ilk kod ne?

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -2,6 +2,7 @@ package JAVAPROJEKT.javaclasses;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Yine bir klasik olarak,Merhaba Dünya");
         
     }
 }
