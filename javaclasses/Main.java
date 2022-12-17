3f59973cd365e41954db06e570e459464a2550bc
oguzhanzeybek <darkstroge@gmail.com>
2022-12-18 02:40:51 +0300

    ilk degiskenlerim

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -2,7 +2,14 @@ package JAVAPROJEKT.javaclasses;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Yine bir klasik olarak,Merhaba Dünya");
+        int sayi = 10;
+        System.out.println("Yine bir klasik olarak,Merhaba Dünya");  
+        //java case sensitive dir.
+        System.out.println("ve yine basliyoruz oynat bakalİm.. sjdnvksdnvs");
+        System.out.println(sayi);
+        int sayiSayiSayi = 99; // DEGİŞKEN İSİMLERİ BU SEKILDE YAZILIR
+        System.out.println("ogrenci sayim:" +sayiSayiSayi);
+
         
     }
 }
