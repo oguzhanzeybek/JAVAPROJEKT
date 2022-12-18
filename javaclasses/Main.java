093e6b36109d4e5ef8c6332899df0b1ad46f1098
oguzhanzeybek <darkstroge@gmail.com>
2022-12-18 03:44:50 +0300

    else if
    skldm..

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -5,10 +5,16 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        int sayi=9;
+        int sayi=30;
         if(sayi>30){
             System.out.println("sayi 30 dan büyük");
-        }else{
+        }else if(sayi==30){
+            System.out.println("sayi 30 a esit");
+        }
+        
+        
+        
+        else{
             System.out.println("sayi 30 dan küçük");
         }
         
