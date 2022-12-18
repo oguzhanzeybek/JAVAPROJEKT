fce88cc99fabb33a9da95be7e025140242250731
oguzhanzeybek <darkstroge@gmail.com>
2022-12-18 03:42:34 +0300

    if-else

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -1,22 +1,16 @@
 package JAVAPROJEKT.javaclasses;
 
 public class Main {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        String mesaj = "ogrenci sayisi";
-        int sayi = 103132444;
-        byte karakter =127;
-        System.out.println(+karakter);  
-
-        System.out.println("Yine bir klasik olarak,Merhaba Dünya");  
-        //java case sensitive dir.
-        System.out.println("ve yine basliyoruz oynat bakalİm.. sjdnvksdnvs");
-        System.out.println(sayi);
-        int sayiSayiSayi = 99; // DEGİŞKEN İSİMLERİ BU SEKILDE YAZILIR
-        System.out.println(mesaj +sayiSayiSayi); //bu sekılde eklenır.
-        double sayiiii= 12.5;
-        System.out.print(sayiiii);
-
-
+        int sayi=9;
+        if(sayi>30){
+            System.out.println("sayi 30 dan büyük");
+        }else{
+            System.out.println("sayi 30 dan küçük");
+        }
         
     }
 }
