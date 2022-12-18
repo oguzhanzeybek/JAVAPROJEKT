652361c706ab1ce3bf25bcff32d9da99059442b4
oguzhanzeybek <darkstroge@gmail.com>
2022-12-18 17:03:59 +0300

    for

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -1,31 +1,14 @@
 package JAVAPROJEKT.javaclasses;
 
 public class Main {
-    /**
-     * @param args
-     */
+
     public static void main(String[] args) {
-        char not = 'D';
-
-        switch (not) {
-            case 'A':
-                System.out.println("mukemmel:geçtiniz");
-                break;
-            case 'B':
-                System.out.println("İDARE EDER:geçtiniz");
-                break;
-            case 'C':
-                System.out.println("ZORLAYA ZORLAYA:geçtiniz");
-                break;
-            case 'D':
-                System.out.println("AZ DAHA KALIYODUN:geçtiniz");
-                break;
-            case 'F':
-                System.out.println("HAHAHAHHA:KALDIN!!!");
-                break;
-            default:
-                System.out.println("gecersiz not girildi");
-
+        // İ=İ+1 == i++
+        //i = i + 5 == i+=2
+        // for(int i=1;i<90; i++){
+        for (int i = 0; i <= 90;  i+=2) {
+            System.out.println(i);
         }
+        System.out.println("döngü bitti");
     }
 }
