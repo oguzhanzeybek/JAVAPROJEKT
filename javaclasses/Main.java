b47c1743dc50f9872abc76dc861e9593261e1db0
oguzhanzeybek <darkstroge@gmail.com>
2022-12-18 03:17:55 +0300

    değişkenler

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -2,13 +2,20 @@ package JAVAPROJEKT.javaclasses;
 
 public class Main {
     public static void main(String[] args) {
-        int sayi = 10;
+        String mesaj = "ogrenci sayisi";
+        int sayi = 103132444;
+        byte karakter =127;
+        System.out.println(+karakter);  
+
         System.out.println("Yine bir klasik olarak,Merhaba Dünya");  
         //java case sensitive dir.
         System.out.println("ve yine basliyoruz oynat bakalİm.. sjdnvksdnvs");
         System.out.println(sayi);
         int sayiSayiSayi = 99; // DEGİŞKEN İSİMLERİ BU SEKILDE YAZILIR
-        System.out.println("ogrenci sayim:" +sayiSayiSayi);
+        System.out.println(mesaj +sayiSayiSayi); //bu sekılde eklenır.
+        double sayiiii= 12.5;
+        System.out.print(sayiiii);
+
 
         
     }
