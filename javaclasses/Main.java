4d519ace7239b86f9a50fbbd2c2540dab4a8f5d2
oguzhanzeybek <darkstroge@gmail.com>
2022-12-18 17:11:14 +0300

    while

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -4,11 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         // İ=İ+1 == i++
-        //i = i + 5 == i+=2
+        // i = i + 5 == i+=2
         // for(int i=1;i<90; i++){
-        for (int i = 0; i <= 90;  i+=2) {
+        for (int i = 0; i <= 90; i += 2) {
             System.out.println(i);
+
         }
-        System.out.println("döngü bitti");
+        System.out.println("for dongusu bitti");
+
+        int i = 1;
+        while(i<100){
+            System.out.println(i);
+            i+=5;
+        } System.out.println(" while dongusu bitti");
+
     }
+
 }
