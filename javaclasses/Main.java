ac53aca461c7d2e55656c307c7f27bd807e66d70
oguzhanzeybek <darkstroge@gmail.com>
2022-12-19 23:50:01 +0300

    total number(list)

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
        double[] mylist={1.2,1.3,1.4,1.5,1.6};
+       double total=0;
 
        for(double number:mylist){
+        total=total + number;
         System.out.println(number);
        }
+       System.out.println("toplam:"+total);
 
     }
 }
