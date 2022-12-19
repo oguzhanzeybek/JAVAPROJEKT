691f9bd75859a0b6c4861769b422da4ac2fc15c3
oguzhanzeybek <darkstroge@gmail.com>
2022-12-20 00:08:15 +0300

    for
    skdlm....

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -3,19 +3,27 @@ package JAVAPROJEKT.javaclasses;
 public class Main {
 
     public static void main(String[] args) {
-       double[] mylist={1.2,1.3,1.4,1.5,1.6};
-       double total=0;
-       double max=mylist[0];
+        String[][] sayi = new String[3][3];
 
-       for(double number:mylist){
-        if(max<number){
-            max =number;
+        sayi[0][0]= "1";
+        sayi[0][1]= "2";
+        sayi[0][2]= "3";
+        sayi[1][0]= "4";
+        sayi[1][1]= "5";
+        sayi[1][2]= "6";
+        sayi[2][0]= "7";
+        sayi[2][1]= "8";
+        sayi[2][2]= "9";
+
+        for(int i =0; i<=2; i++){
+            System.out.println("__________   ___________");
+            for(int j=0; j<=2; j++){
+                System.out.println(sayi[i][j]);
+            }
         }
-        total=total + number;
-        System.out.println(number);
-       }
-       System.out.println("toplam:"+total);
-       System.out.println("EN BUYUK SAYI:"+max);
+
 
     }
+
+
 }
