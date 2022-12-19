67cd9578206646c62e60a15b10c4276cedde8a30
oguzhanzeybek <darkstroge@gmail.com>
2022-12-20 00:33:07 +0300

    Strings

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -3,27 +3,25 @@ package JAVAPROJEKT.javaclasses;
 public class Main {
 
     public static void main(String[] args) {
-        String[][] sayi = new String[3][3];
+        String mesaj = "bugun hava cok sicak";
 
-        sayi[0][0]= "1";
-        sayi[0][1]= "2";
-        sayi[0][2]= "3";
-        sayi[1][0]= "4";
-        sayi[1][1]= "5";
-        sayi[1][2]= "6";
-        sayi[2][0]= "7";
-        sayi[2][1]= "8";
-        sayi[2][2]= "9";
+        System.out.println(mesaj);
 
-        for(int i =0; i<=2; i++){
-            System.out.println("__________   ___________");
-            for(int j=0; j<=2; j++){
-                System.out.println(sayi[i][j]);
-            }
-        }
+        System.out.println("eleman sayisi:" + mesaj.length());
+        System.out.println("5.eleman:" + mesaj.charAt(4));
+        System.out.println(mesaj.concat(",yasaaaaaaaaaaasin mi acaba?"));
+        System.out.println(mesaj.startsWith("A"));
+        System.out.println(mesaj.startsWith("b"));
+        System.out.println(mesaj.endsWith("A"));
+        System.out.println(mesaj.endsWith("k"));
 
+        char[] karakterler = new char[5];
+        mesaj.getChars(0, 5, karakterler, 0);
+        System.out.println(karakterler);
+
+        System.out.println(mesaj.indexOf("a"));// ilk buldugunu getirir
+        System.out.println(mesaj.lastIndexOf("k")); 
 
     }
-
 
 }
