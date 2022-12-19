761485433b057eecbacd26d43cbc9ff56a5a3b1f
oguzhanzeybek <darkstroge@gmail.com>
2022-12-19 23:54:53 +0300

    if- list

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -5,12 +5,17 @@ public class Main {
     public static void main(String[] args) {
        double[] mylist={1.2,1.3,1.4,1.5,1.6};
        double total=0;
+       double max=mylist[0];
 
        for(double number:mylist){
+        if(max<number){
+            max =number;
+        }
         total=total + number;
         System.out.println(number);
        }
        System.out.println("toplam:"+total);
+       System.out.println("EN BUYUK SAYI:"+max);
 
     }
 }
