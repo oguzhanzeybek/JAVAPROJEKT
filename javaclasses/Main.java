6d0eea2794fbe7f76bfda674c4a4fb1d9e89b4b5
oguzhanzeybek <darkstroge@gmail.com>
2022-12-19 23:45:53 +0300

    List

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -3,30 +3,11 @@ package JAVAPROJEKT.javaclasses;
 public class Main {
 
     public static void main(String[] args) {
-        // İ=İ+1 == i++
-        // i = i + 5 == i+=2
-        // for(int i=1;i<90; i++){
-        for (int i = 0; i <= 90; i += 2) {
-            System.out.println(i);
+       double[] mylist={1.2,1.3,1.4,1.5,1.6};
 
-        }
-        System.out.println("for dongusu bitti");
-
-        int i = 1;
-        while (i < 100) {
-            System.out.println(i);
-            i += 5;
-        }
-        System.out.println(" while dongusu bitti");
-
-        int a = 100;
-        do {
-
-            System.out.println(a);
-            a += 2;
-
-        } while (a < 50);
-        System.out.println("do- while dongusu bitti");
+       for(double number:mylist){
+        System.out.println(number);
+       }
 
     }
 }
