3fe8fc4c896e89a040ac56be0fd115ab7846f87a
oguzhanzeybek <darkstroge@gmail.com>
2022-12-21 04:14:26 +0300

    exercise1
    Prime Number(Primzahl)

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -2,38 +2,35 @@ package JAVAPROJEKT.javaclasses;
 
 public class Main {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        String mesaj = "bugun hava cok sicak";
+        int number = 1;
+        if(number<1){
+            System.out.println("gecersiz sayi");
+            return;
+        }
+        if(number==1){
+            System.out.println("sayi asal degildir");
+            return;
+        }
 
-        System.out.println(mesaj);
+        boolean isPrime = true;
+        for (int i = 2; i < number; i++) {
 
-        /*
-         * System.out.println("eleman sayisi:" + mesaj.length());
-         * System.out.println("5.eleman:" + mesaj.charAt(4));
-         * System.out.println(mesaj.concat(",yasaaaaaaaaaaasin mi acaba?"));
-         * System.out.println(mesaj.startsWith("A"));
-         * System.out.println(mesaj.startsWith("b"));
-         * System.out.println(mesaj.endsWith("A"));
-         * System.out.println(mesaj.endsWith("k"));
-         * 
-         * char[] karakterler = new char[5];
-         * mesaj.getChars(0, 5, karakterler, 0);
-         * System.out.println(karakterler);
-         * 
-         * System.out.println(mesaj.indexOf("a"));// ilk buldugunu getirir
-         * System.out.println(mesaj.lastIndexOf("k"));
-         */
-        String yeniMesaj = mesaj.replace(" ", "-");
-        System.out.println(yeniMesaj);
-        System.out.println(mesaj.substring(2,5));
+            if (number % i == 0) {
+                isPrime = false;
 
-        for( String kelime: mesaj.split("")){
-            System.out.println(kelime);
+            }
 
         }
-        System.out.println(mesaj.toLowerCase()); // kelimeleri kucultur.
-        System.out.println(mesaj.toUpperCase()); // kelimeleri buyutur.
-        System.out.println(mesaj.trim()); // boslukları alır(bas ve son).
+        if (isPrime) {
+            System.out.println("sayi asaldir");
+        } else {
+            System.out.println("sayi asal degildir");
+        }
+        
 
     }
 
