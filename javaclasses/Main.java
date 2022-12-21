f3ade0789b4c4514d8844a2c16cbde3af2d56b96
oguzhanzeybek <darkstroge@gmail.com>
2022-12-21 05:16:41 +0300

    exercise5
    friends Number

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -6,23 +6,28 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        int number = 1;
-        int toplam = 0;
-        if(number==1){
-            System.out.println( "mukemmel sayidir >>> "+number);
-            return;
-        }
+        int number1 = 220;
+        int number2 = 284;
+        int toplam1 = 0;
+        int toplam2 = 0;
 
-        for (int i = 1; i < number; i++) {
-            if (number % i == 0) {
-                toplam = toplam + i; // bu sayede her fpr döngüsünde sayıyı toplama eklemiş oluyoruz
+        for (int i = 1; i < number1; i++) {
+            if (number1 % i == 0) {
+                toplam1 = toplam1 + i; // bu sayede her fpr döngüsünde sayıyı toplama eklemiş oluyoruz
 
             }
         }
-        if(number==toplam){
-            System.out.println( "mukemmel sayidir >>> "+number);
-        }else{
-            System.out.println( "mukemmel sayidir degildir >>> "+number);
+        for (int i = 1; i < number2; i++) {
+            if (number2 % i == 0) {
+                toplam2 = toplam2 + i; // bu sayede her fpr döngüsünde sayıyı toplama eklemiş oluyoruz
+
+            }
+        }
+
+        if (number1 == toplam2 && number2 == toplam1) {
+            System.out.println("arkadas sayidir");
+        } else {
+            System.out.println("arkadas sayi degildir");
         }
 
     }
