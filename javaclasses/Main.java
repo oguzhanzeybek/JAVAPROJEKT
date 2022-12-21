63bd074edd9d0d938241b945f5b50d6c6986aae2
oguzhanzeybek <darkstroge@gmail.com>
2022-12-21 04:21:26 +0300

    exercise2
    vowel-Vokal

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -6,30 +6,17 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        int number = 1;
-        if(number<1){
-            System.out.println("gecersiz sayi");
-            return;
-        }
-        if(number==1){
-            System.out.println("sayi asal degildir");
-            return;
-        }
-
-        boolean isPrime = true;
-        for (int i = 2; i < number; i++) {
-
-            if (number % i == 0) {
-                isPrime = false;
-
-            }
-
-        }
-        if (isPrime) {
-            System.out.println("sayi asaldir");
-        } else {
-            System.out.println("sayi asal degildir");
-        }
+       char harf = 'A';
+       switch(harf){
+        case 'A':
+        case 'I':
+        case 'O':
+        case 'U':
+        System.out.print("Kalin sesli harf");
+        break;
+        default:
+        System.out.print("ince sesli harf");
+       }
         
 
     }
