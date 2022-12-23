3b63052bae8a60aa7e4d44811a9f1c8f59c7bbe2
oguzhanzeybek <darkstroge@gmail.com>
2022-12-23 21:36:22 +0300

    methods2

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -1,40 +1,37 @@
 package JAVAPROJEKT.javaclasses;
 
+
+
 public class Main {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-        sayibulmaca();
+        String mesaj="bugun hava cok guzel";
+        System.out.println(mesaj);
+        String yenimesaj=sehirVer();
+        System.out.println(yenimesaj);
+        int sayi =topla(2,4);
+        System.out.println(sayi);
+    }
+    public static void ekle(){
+        System.out.println("eklendi");
 
     }
-
-    public static void sayibulmaca() {
-        int[] sayilar = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int aranan = 8;
-        boolean varMiAcaba = false;
-
-        for (int sayi : sayilar) {
-            if (sayi == aranan) {
-                varMiAcaba = true;
-
-            }
-
-        }
-
-        if (varMiAcaba) {
-            mesajver("aranan sayi listede: "+aranan);
-
-        } else {
-            mesajver("aranan sayi listede degil: : "+aranan);
-
-        }
-
+    public static void sil(){
+        System.out.println("silindi");
     }
-
-    public static void mesajver(String mesaj){
-        System.out.println(mesaj); 
+    public static void guncelle(){
+        System.out.println("guncellendi");
+    }
+    public static int topla(int sayi1,int sayi2){
+        return sayi1+sayi2;
+        
+    }
+    public static String sehirVer(){
+        return "Karaman";
+        
     }
 
 }
