19cde2d2f9c0b3ca92e3a9bfe7d5bc21094395c8
oguzhanzeybek <darkstroge@gmail.com>
2022-12-23 21:45:01 +0300

    methods3

--- javaclasses/Main.java
+++ javaclasses/Main.java
@@ -14,6 +14,8 @@ public class Main {
         System.out.println(yenimesaj);
         int sayi =topla(2,4);
         System.out.println(sayi);
+        int toplam=topla2(1,23,4,5,56,33);
+        System.out.println(toplam);
     }
     public static void ekle(){
         System.out.println("eklendi");
@@ -32,6 +34,13 @@ public class Main {
     public static String sehirVer(){
         return "Karaman";
         
+    }
+    public static int topla2(int... sayilar){
+       int toplam=0;
+       for(int sayi:sayilar){
+        toplam+=sayi;
+       }
+        return toplam;
     }
 
 }
