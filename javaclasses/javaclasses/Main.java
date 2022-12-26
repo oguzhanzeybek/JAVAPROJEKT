bbe4ba2e81f973845e978a05bd36088afb0673e3
oguzhanzeybek <darkstroge@gmail.com>
2022-12-27 01:13:06 +0300

    stack-heap1
    iki fonksiyonun degerleri 1. olana aktarıldı tek fonksıyon gibi çalısıyor.

--- javaclasses/javaclasses/Main.java
+++ javaclasses/javaclasses/Main.java
@@ -21,6 +21,13 @@ public class Main {
        sayi2=30;              //stack de sayi1 sayi2 ye eşit diye okunur.bu yuzden cevap 20 olmustur.
        System.out.println(sayi1);
 
+       int[] sayilar1= new int[]{1,2,3};
+       int[] sayilar2= new int[]{9,8,7};
+       sayilar2=sayilar1;
+       sayilar1[0]=99;
+       System.out.println(sayilar2[0]);
+
+
 
 
 
