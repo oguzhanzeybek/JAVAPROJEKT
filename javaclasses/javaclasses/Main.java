5d72efa40caedb8a60643e5cb6328761d18ab129
oguzhanzeybek <darkstroge@gmail.com>
2022-12-26 22:58:48 +0300

    object(gc)

--- javaclasses/javaclasses/Main.java
+++ javaclasses/javaclasses/Main.java
@@ -8,7 +8,9 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-       CustomerManager musteriyappici = new CustomerManager();
+       CustomerManager musteriyappici = new CustomerManager();//buranın ismi hep değişebilir(musteriyappici)
+       CustomerManager musteriyappici2= new CustomerManager();
+       musteriyappici=musteriyappici2;// bir tanesi görmezden gelinir
        musteriyappici.Add("kjsnckasjcnkasjcnaksjc");
        musteriyappici.Add();
        musteriyappici.Add(49586);
