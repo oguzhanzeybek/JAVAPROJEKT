f125b7e534d183eec3ac1baddee4c375a88ffc92
oguzhanzeybek <darkstroge@gmail.com>
2022-12-29 22:38:48 +0300

    white papier :)

--- javaclasses2/Main.java
+++ javaclasses2/Main.java
@@ -1,11 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Product product = new Product(1);
-        product.name = "Laptop";
-
-        ProduckManager produckManager = new ProduckManager();
-        produckManager.Add(product);
+       
     }
 
 }
--- javaclasses2/ProduckManager.java
+++
@@ -1,7 +0,0 @@
-public class ProduckManager {
-    public void Add(Product product) {
-        // JDBC
-        System.out.println("urun eklendi " + product.name);
-    }
-
-}
