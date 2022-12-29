36e1a29fde33a18767437620041ed3f6e23fa810
oguzhanzeybek <darkstroge@gmail.com>
2022-12-29 21:36:28 +0300

    set-get

--- javaclasses2/ProduckManager.java
+++ javaclasses2/ProduckManager.java
@@ -1,8 +1,7 @@
 public class ProduckManager {
-    public void Add(Product product){
-       //JDBC
-       System.out.println("urun eklendi " +product.name);
+    public void Add(Product product) {
+        // JDBC
+        System.out.println("urun eklendi " + product.name);
     }
 
-   
 }
--- javaclasses2/Product.java
+++ javaclasses2/Product.java
@@ -1,7 +1,14 @@
 public class Product {
-   private int id;
-   private String name;
-   private String description;   // attribut-field
-   private double price;
-   private int StockAmount;
+  private int _id;
+  private String name;
+  private String description; // attribut-field
+  private double price;
+  private int StockAmount;
+
+  public int getid(){
+    return _id;
+  }
+  public void setId(int id){
+    _id= id;
+  }
 }
