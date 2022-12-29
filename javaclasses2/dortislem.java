3824229a2651ebf600d1de918d9f4cff4c8e15f9
oguzhanzeybek <darkstroge@gmail.com>
2022-12-29 22:48:11 +0300

    overloading2

--- javaclasses2/dortislem.java
+++ javaclasses2/dortislem.java
@@ -2,4 +2,7 @@ public class dortislem {
     public int topla(int sayi1,int sayi2){
         return sayi1+sayi2;
     }
+    public int topla(int sayi1,int sayi2,int sayi3){
+        return sayi1+sayi2+sayi3;
+    }
 }
