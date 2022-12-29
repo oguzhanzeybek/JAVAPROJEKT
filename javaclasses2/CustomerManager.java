91c06f8a87d16d2894084a83fc36a0fd4cd0dca5
oguzhanzeybek <darkstroge@gmail.com>
2022-12-29 23:11:50 +0300

    inheritance2

---
+++ javaclasses2/CustomerManager.java
@@ -0,0 +1,8 @@
+public class CustomerManager {
+    public void List(){
+        System.out.println(" listelendi");
+    }
+    public void Add(){
+        System.out.println(" listelendi");
+    }
+}
---
+++ javaclasses2/EmployeeManager.java
@@ -0,0 +1,12 @@
+public class EmployeeManager {
+    public void List() {
+        System.out.println(" listelendi");
+    }
+
+    public void Add() {
+        System.out.println(" listelendi");
+    } 
+    public void BestEmployee() {
+        System.out.println(" ayin elemani getirildi");
+    } 
+}
