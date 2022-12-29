4690815d6970302a3aaf788defd8511c540a5935
oguzhanzeybek <darkstroge@gmail.com>
2022-12-29 22:47:15 +0300

    overloading1

--- javaclasses2/Main.java
+++ javaclasses2/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-       
+        dortislem dortislem=new dortislem();
+        System.out.println(dortislem.topla(2, 3));
     }
 
 }
---
+++ javaclasses2/dortislem.java
@@ -0,0 +1,5 @@
+public class dortislem {
+    public int topla(int sayi1,int sayi2){
+        return sayi1+sayi2;
+    }
+}
