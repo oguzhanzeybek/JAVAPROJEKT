b0cd0eb35575a5ae08a5abb75e740fcb3044958d
oguzhanzeybek <darkstroge@gmail.com>
2022-12-29 22:50:10 +0300

    inheritance1

--- javaclasses2/Main.java
+++ javaclasses2/Main.java
@@ -1,9 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        dortislem dortislem=new dortislem();
-        System.out.println(dortislem.topla(2, 3));
-        System.out.println(dortislem.topla(2, 3,5));
+        
     }
 
 }
