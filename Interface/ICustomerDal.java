071bcf98f91b7e37a59091ff687230b6d961c4f7
oguzhanzeybek <darkstroge@gmail.com>
2022-12-31 00:50:04 +0300

    Interface2

--- Interface/ICustomerDal.java
+++ Interface/ICustomerDal.java
@@ -1,3 +1,3 @@
 public interface ICustomerDal {
-    
+    void Add();
 }
---
+++ Interface/MySqlCustomerDal.java
@@ -0,0 +1,6 @@
+public class MySqlCustomerDal implements ICustomerDal {
+
+    @Override
+    public void Add() {} //eklemesi zorunludur.
+    
+}
