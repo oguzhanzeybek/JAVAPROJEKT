fa2d22eb1f0bf68e3c3256f43b1acc8dece6c760
oguzhanzeybek <darkstroge@gmail.com>
2022-12-30 15:47:57 +0300

    Overreding2

--- javaclasses2/Main.java
+++ javaclasses2/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        OgretmenKrediManager ogretmenKrediManager=new OgretmenKrediManager();
+        System.out.println(ogretmenKrediManager.hesapla(3842));;
 
     }
 }
--- javaclasses2/OgretmenKrediManager.java
+++ javaclasses2/OgretmenKrediManager.java
@@ -1,3 +1,3 @@
-public class OgretmenKrediManager {
+public class OgretmenKrediManager extends BaseKrediManager{
     
 }
