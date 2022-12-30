ca9f2e4867b31af42321ed5bb65a486ea12034ec
oguzhanzeybek <darkstroge@gmail.com>
2022-12-30 16:05:48 +0300

    Overriding4

--- javaclasses2/BaseKrediManager.java
+++ javaclasses2/BaseKrediManager.java
@@ -1,5 +1,6 @@
 public class BaseKrediManager {
-    public double hesapla(double tutar) {
+    public final double hesapla(double tutar) {
      return tutar * 1.18;//1.18 faiz oranimiz.
     }
 }
+//final kullanılırsa ustune yazılamaz.OgrenciKrediManagerda oldugu gıbı hata verır.
--- javaclasses2/OgrenciKrediManager.java
+++ javaclasses2/OgrenciKrediManager.java
@@ -1,3 +1,6 @@
 public class OgrenciKrediManager  extends BaseKrediManager {
-    
+    public double hesapla(double tutar) {
+        return tutar * 1.08;//1.08 faiz oranimiz.
+       }
+    //artık bunu kullanıyor.Override etmıs olduk.
 }
