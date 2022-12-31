91c9f555cbe3fc11146b868c2757cdc09a40c4d3
oguzhanzeybek <darkstroge@gmail.com>
2022-12-31 14:02:51 +0300

    soru1.1

--- soru1/Main.java
+++ soru1/Main.java
@@ -1,19 +1,28 @@
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        try (Scanner scan = new Scanner(System.in)) {
-            scan.useLocale(Locale.US);
-            System.out.println("cemberin yaricapini giriniz:");
+        Scanner scan = new Scanner(System.in);
 
-            double yaricap = scan.nextDouble();
-            System.out.println("cemberin yaricapi:"+yaricap);
-            System.out.println("cemberin capi:"+yaricap*2);
-            System.out.println("cemberin cevresi: pi."+yaricap*2 );
-            System.out.println("cemberin alani: pi."+yaricap*+yaricap);
-        }
+        
+        scan.useLocale(Locale.US);
+        System.out.println("cemberin yaricapini giriniz:");
+
+        double yaricap = scan.nextDouble();
+        System.out.println("cemberin yaricapi:"+yaricap);
+        System.out.println("cemberin capi:"+yaricap*2);
+        System.out.println("cemberin cevresi: pi."+yaricap*2 );
+        System.out.println("cemberin alani: pi."+yaricap*+yaricap);
+        scan.close();
+        
+
+
+
+        
+        
 
 
 
