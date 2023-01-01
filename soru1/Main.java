951329f9e0a68cfb5be69b115ca5f245d5ecb77d
oguzhanzeybek <darkstroge@gmail.com>
2023-01-01 23:50:01 +0300

    Mutlu Yıllaaar :)

--- soru1/Main.java
+++ soru1/Main.java
@@ -1,31 +1,12 @@
 
-import java.util.Locale;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-
-        
-        scan.useLocale(Locale.US);
-        System.out.println("cemberin yaricapini giriniz:");
-
-        double yaricap = scan.nextDouble();
-        System.out.println("cemberin yaricapi:"+yaricap);
-        System.out.println("cemberin capi:"+yaricap*2);
-        System.out.println("cemberin cevresi: pi."+yaricap*2 );
-        System.out.println("cemberin alani: pi."+yaricap*+yaricap);
-        scan.close();
-        
-
-
-
-        
-        
-
-
-
+    for(int i = 0;i<2024; i++){
+    System.out.println("Mutlu Yillaaaaarrr.İşte başliyoruzzz." +i);        
+    }
 
     }
 }
